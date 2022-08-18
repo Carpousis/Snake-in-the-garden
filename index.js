adc9ee8b7c3753a6c05396230f3923bce9a29c23
Christopher Carpousis <carpousis@gmail.com>
2022-08-18 04:11:02 -0400

    no snake or apple appears

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-let grid = document.querySelector(".grid")
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("keyup", control);
-    createBoard();
-    startGame();
-    playAgain.addEventListener("click", replay)
-})
-
-const createBoard = () => {
-    popup.style.display = "none";
-        for (let i = 0; i < 100; i++) {
-            let div = document.createElement("div");
-            grid.appendChild(div);
-        }
-}
-
-let popup = document.querySelector(".popup")
-
-let playAgain = document.querySelector(".playAgain")
-let scoreDisplay = document.querySelector(".scoreDisplay")
-
-let top = document.querySelector(".top")
-let right = document.querySelector(".right")
-let  bottom= document.querySelector(".bottom")
-let left = document.querySelector(".left")
-
+let grid = document.querySelector(".grid");
+let popup = document.querySelector(".popup");
+let playAgain = document.querySelector(".playAgain");
+let scoreDisplay = document.querySelector(".scoreDisplay");
+let left = document.querySelector(".left");
+let bottom = document.querySelector(".bottom");
+let right = document.querySelector(".right");
+let up = document.querySelector(".top");
 let width = 10;
 let currentIndex = 0; 
 let appleIndex = 0;
@@ -35,7 +16,14 @@ let speed = 0.8;
 let intervalTime = 0;
 let interval = 0;
 
-const startGame = () => {
+function createBoard(){
+    popup.style.display = "none";
+        for (let i = 0; i < 100; i++) {
+            let div = document.createElement("div");
+            grid.appendChild(div);
+    }
+}
+function startGame () {
     let squares = document.querySelectorAll(".grid div");
     randomApple(squares);
     direction = 1;
@@ -44,10 +32,16 @@ const startGame = () => {
     currentSnake = [2, 1, 0];
     currentIndex = 0;
         currentSnake.forEach((index) => squares[index].classList.add("snake"));
-    interval = setInterval(moveOutome, intervalTime);
-}
+       interval = setInterval(moveOutome, intervalTime);
 
-const moveOutcome() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("keyup", control);
+    createBoard(currentSnake);
+    startGame();
+    playAgain.addEventListener("click", replay);
+    })
+}
+function moveOutcome () {
     let squares = document.querySelectorAll(".grid div");
     if (checkForHits(squares)) {
         alert("Hit Something");
@@ -58,16 +52,16 @@ const moveOutcome() {
     }
 }
 
-const moveSnake = (squares) => {
+function moveSnake (squares) {
     let tail = currentSnake.pop();
     squares[tail].classList.remove("snake");
-    currentSnake.unshift(currentSnake[0] = direction);
+    currentSnake.unshift(currentSnake[0] + direction)
     eatApple(squares, tail);
     squares[currentSnake[0]].classList.add("snake");
 }
 
-const checkForHits = (squares) => {
-    if(
+function checkForHits (squares) {
+    if (
         (currentSnake[0] + width >= width * width && direction === width) ||
         (currentSnake[0] % width === width - 1 && direction === 1) ||
         (currentSnake[0] % width === 0 && direction === -1) ||
@@ -80,7 +74,7 @@ const checkForHits = (squares) => {
     }
 }
 
-const eatApple(squares, tail) {
+function eatApple (squares, tail) {
     if (squares[currentSnake[0]].classList.contains("apple")) {
         squares[currentSnake[0]].classList.remove("apple");
         squares[tail].classList.add("snake");
@@ -94,14 +88,14 @@ const eatApple(squares, tail) {
     }
 }
 
-const randomApple = (squares) => {
+function randomApple (squares) {
     do {
         appleIndex = Math.floor(Math.random() * squares.length);
     } while (squares[appleIndex].classList.contains("snake"));
     squares[appleIndex].classList.add("apple");
 }
 
-const control = (e) => {
+function control (e) {
     if (e.keycode === 39) {
         direction = 1;
     } else if (e.keycode === 38) {
